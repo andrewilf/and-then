9915a3650cfe74ce8f4d21e5af739425e80bf386
andrewfaulkner <andrewfau1kn3r@gmail.com>
2022-02-12 13:46:19 +0800

    tweaked scroll

--- src/App.js
+++ src/App.js
@@ -39,6 +39,9 @@ function App() {
       }
     }
     updateQuote();
+    // return () => {
+
+    // }
   }, []);
   return (
     <AppShell
--- src/components/EditPromptPage.jsx
+++ src/components/EditPromptPage.jsx
@@ -26,9 +26,9 @@ const EditPromptSection = () => {
   const [imageURL, setimageURL] = useState(
     "https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
   );
-  const [scroll, setScroll] = useWindowScroll();
 
   useEffect(() => {
+    const [scroll, setScroll] = useWindowScroll();
     setScroll({ y: 0 });
   }, []);
   return (
